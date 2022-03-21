a79c0a239c4732e57f3d87ab8d2bb16777ce1fcd
tonyd2020 <tonyd2ipad@gmail.com>
2022-03-21 17:47:02 +1100

    Enabled auto landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,8 @@ class _MyHomePageState extends State<MyHomePage> {
         id: 't2', amount: 239.99, date: DateTime.now(), title: 'Groceries'),
   ];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((element) {
       return element.date.isAfter(
@@ -84,34 +86,74 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _deleteTransaction(String id){
+  void _deleteTransaction(String id) {
     setState(() {
       _userTransactions.removeWhere((element) {
         return element.id == id;
       });
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final appBar = AppBar(
+      title: Text('Personal Expenses'),
+      actions: [
+        IconButton(
+          onPressed: () => _startAddNewTransaction(context),
+          icon: Icon(Icons.add),
+        ),
+      ],
+    );
+
+
+final txListWidget = Container(
+  height: (MediaQuery.of(context).size.height -
+      appBar.preferredSize.height -
+      MediaQuery.of(context).padding.top) *
+      0.7,
+  child: TransactionList(_userTransactions, _deleteTransaction),
+);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Personal Expenses'),
-        actions: [
-          IconButton(
-            onPressed: () => _startAddNewTransaction(context),
-            icon: Icon(Icons.add),
-          ),
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Chart(recentTransactions: _recentTransactions),
-            TransactionList(_userTransactions,_deleteTransaction)
+            if (isLandscape) Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Show Chart'),
+                Switch(
+                    value: _showChart,
+                    onChanged: (val) {
+                      setState(() {
+                        _showChart = val;
+                      });
+                    }),
+              ],
+            ),
+
+            if(!isLandscape) Container(
+              height: (MediaQuery.of(context).size.height -
+                  appBar.preferredSize.height -
+                  MediaQuery.of(context).padding.top) *
+                  0.3,
+              child: Chart(recentTransactions: _recentTransactions),
+            ),
+
+            if(!isLandscape) txListWidget,
+
+            _showChart ? Container(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.7,
+              child: Chart(recentTransactions: _recentTransactions),
+            ) : txListWidget,
           ],
         ),
       ),
